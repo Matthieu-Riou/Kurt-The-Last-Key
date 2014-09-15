0f920b32507c186475e02b4d604c0994fe644200
Matthieu-Riou <matthieu.riou@etu.univ-nantes.fr>
2014-09-15 17:22:42 +0200

    Add a title to the introduction

--- Source/Menu/Intro.cpp
+++ Source/Menu/Intro.cpp
@@ -3,6 +3,8 @@
 void intro(sf::RenderWindow &app)
 {
 	sf::RectangleShape fond_;
+	sf::Font font_;
+	sf::Text texte_;
 
 	fond_.setSize(sf::Vector2f(Propriete::Fenetre::fenX(), Propriete::Fenetre::hauteurSol()));
 	fond_.setFillColor(sf::Color::Cyan);
@@ -11,6 +13,17 @@ void intro(sf::RenderWindow &app)
 
 	decor_.push_back(new Arbre(50));
 	decor_.push_back(new Nuage(Propriete::Fenetre::fenX()));
+
+	font_.loadFromFile("Font/arial.ttf");
+	texte_.setString("Kurt");
+	texte_.setFont(font_);
+	texte_.setColor(sf::Color::Red);
+	texte_.setCharacterSize(150);
+	texte_.setStyle(sf::Text::Bold);
+
+	sf::FloatRect rect = texte_.getLocalBounds();
+	texte_.setPosition(sf::Vector2f((Propriete::Fenetre::fenX() - rect.width) / 2, (Propriete::Fenetre::fenY() - Propriete::Fenetre::hauteurSol() - rect.height/2)));
+
 
 	while(!(sf::Keyboard::isKeyPressed(sf::Keyboard::Space)))
 	{
@@ -42,6 +55,8 @@ void intro(sf::RenderWindow &app)
 
 		for(std::list<Decor*>::const_iterator it = decor_.begin(); it != decor_.end(); ++it)
 			(*it)->afficher(app);
+
+		app.draw(texte_);
 
         app.display();
 	}
